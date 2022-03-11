cd8bf7b39b5cc46a6242e6c9d4c02014f8bfc1ef
Jason Witzemann <jason.witzemann@students.fhv.at>
2022-03-11 16:08:02 +0100

    testing

--- src/main/java/at/fhv/ae/Main.java
+++ src/main/java/at/fhv/ae/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory em = Persistence.createEntityManagerFactory("Test");
+        EntityManagerFactory em = Persistence.createEntityManagerFactory("test");
         EntityManager eManager = em.createEntityManager();
 
         try {
