a371dc0a53f30424cbb0ca079c2ef417f1e432c9
Jason Witzemann <jason.witzemann@students.fhv.at>
2022-03-11 15:40:00 +0100

    postgres db anschluss

--- src/main/java/at/fhv/ae/Main.java
+++ src/main/java/at/fhv/ae/Main.java
@@ -1,7 +1,26 @@
 package at.fhv.ae;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        EntityManagerFactory em = Persistence.createEntityManagerFactory("Test");
+        EntityManager eManager = em.createEntityManager();
+
+        try {
+            eManager.getTransaction().begin();
+            eManager.createNativeQuery("CREATE TABLE Persons (\n" +
+                    "    PersonID int,\n" +
+                    "    LastName varchar(255),\n" +
+                    "    FirstName varchar(255),\n" +
+                    ");");
+            eManager.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
