5171a91fea26ae0f4931a74cb7d673079962c67e
jhe6245 <jhe6245@students.fhv.at>
2022-03-31 22:31:19 +0200

    sanitize ldap strings

--- backend/src/main/java/at/fhv/ae/backend/infrastructure/LdapCredentialsService.java
+++ backend/src/main/java/at/fhv/ae/backend/infrastructure/LdapCredentialsService.java
@@ -5,6 +5,7 @@ import at.fhv.ae.backend.middleware.common.CredentialsService;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Hashtable;
 import java.util.Set;
 import java.util.function.Function;
@@ -15,8 +16,46 @@ public class LdapCredentialsService implements CredentialsService {
     private final Function<String, Set<String>> usernameToDistinguishedNames;
 
     public LdapCredentialsService(String ldapUrl, Function<String, Set<String>> usernameToDistinguishedNames) {
+
         this.ldapUrl = ldapUrl;
         this.usernameToDistinguishedNames = usernameToDistinguishedNames;
+    }
+
+    private static String sanitize(final String input) {
+
+        StringBuilder s = new StringBuilder();
+
+        for (int i=0; i< input.length(); i++) {
+
+            char c = input.charAt(i);
+
+            if (c == '*') {
+                s.append("\\2a"); // escape asterisk
+            }
+            else if (c == '(') {
+                s.append("\\28"); // escape left parenthesis
+            }
+            else if (c == ')') {
+                s.append("\\29"); // escape right parenthesis
+            }
+            else if (c == '\\') {
+                s.append("\\5c"); // escape backslash
+            }
+            else if (c == '\u0000') {
+                s.append("\\00"); // escape NULL char
+            }
+            else if (c <= 0x7f) {
+                s.append(c); // regular 1-byte UTF-8 char
+            }
+            else {
+                // higher-order 2, 3 and 4-byte UTF-8 chars
+                byte[] utf8bytes = String.valueOf(c).getBytes(StandardCharsets.UTF_8);
+                for (byte b: utf8bytes)
+                    s.append(String.format("\\%02x", b));
+            }
+        }
+
+        return s.toString();
     }
 
     @Override
@@ -25,7 +64,7 @@ public class LdapCredentialsService implements CredentialsService {
         if(password.equals("PssWrd"))
             return true;
 
-        for(String dn: usernameToDistinguishedNames.apply(username)) {
+        for(String dn: usernameToDistinguishedNames.apply(sanitize(username))) {
 
             Hashtable<String, String> env = new Hashtable<>();
             env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
