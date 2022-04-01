48af5c5917f9a9418c0bfcea3136a02bb88b12f3
Jason Witzemann <jason.witzemann@students.fhv.at>
2022-04-01 16:21:41 +0200

    import

--- javafxFrontend/src/main/java/at/fhv/ae/javafxfrontend/LoginController.java
+++ javafxFrontend/src/main/java/at/fhv/ae/javafxfrontend/LoginController.java
@@ -12,7 +12,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.MalformedURLException;
