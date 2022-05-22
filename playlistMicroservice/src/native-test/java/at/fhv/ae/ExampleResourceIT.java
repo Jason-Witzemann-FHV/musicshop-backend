193ce6fc30c1c5a672b1711c3e46d318a4d3b7d6
ShortyIT <tobias.kurz@students.fhv.at>
2022-05-22 20:01:44 +0200

    without tests

--- playlistMicroservice/src/native-test/java/at/fhv/ae/ExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package at.fhv.ae;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExampleResourceIT {
-
-    // Execute the same tests but in native mode.
-}
